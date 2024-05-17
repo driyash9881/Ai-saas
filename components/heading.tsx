12297ce24ea9d735f4ace4811d220e6101195eff
driyash9881 <priyatiwari9881@gmail.com>
2024-05-17 14:09:14 +0530

    lets check if it works

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,26 +1,24 @@
 import { XIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-
 interface HeadingProps {
   title: string;
   description: string;
-  Icon: XIcon;
+  Icon: typeof XIcon;
   iconColor?: string;
   bgColor?: string;
 }
 
-
-export const Heading=({
-    title,
-    description,
-    Icon :Icon,
-    iconColor,
-    bgColor,
-}: HeadingProps)=>{
-    return(
-        <div>
-             <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+export const Heading = ({
+  title,
+  description,
+  Icon,
+  iconColor,
+  bgColor,
+}: HeadingProps) => {
+  return (
+    <div>
+      <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
         <div className={cn("p-2 w-fit rounded-md", bgColor)}>
           <Icon className={cn("w-10 h-10", iconColor)} />
         </div>
@@ -31,6 +29,6 @@ export const Heading=({
           </p>
         </div>
       </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
